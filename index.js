80a4f06104a35cc5069da7c3a69ef7a10dcd51b3
kevin903476 <73252529+kevin903476@users.noreply.github.com>
2024-07-25 20:30:43 -0600

    prueba email
    prueba prueba

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function htmlMessage(promedio, carrera){
             <h2 class="subtitle">A continuación, te presentamos el promedio de tus ejercicios en la carrera de:</h2>
             <br>
             <h2 class="subtitle">${carrera}:</h2>
-            <div class="average"> ${promedio} / 100</div>
+            <div class="average">${promedio}</div>
         </div>
     </body>
     </html>
@@ -116,7 +116,7 @@ function htmlMessageGEC(promedio, carrera){
             <h2 class="subtitle">A continuación, te presentamos la retroalimentación de tu ejercicio en la carrera de:</h2>
             <br>
             <h2 class="subtitle">${carrera}:</h2>
-            <div class="average"> ${promedio} / 100</div>
+            <div class="average">${promedio}</div>
             <p>${mensaje}</p>
         </div>
     </body>
